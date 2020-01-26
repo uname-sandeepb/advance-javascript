c9ccf9eeef1f040b85630c429f516980284ddf65
uname-sandeepb <59410723+uname-sandeepb@users.noreply.github.com>
2020-01-26 11:36:59 +0530

    numbers in javascript

---
+++ primitive-types/number.js
@@ -0,0 +1,42 @@
+/* ------------------------------------------ 
+JavaScript has ONE Number type 
+
+Positive numbers!
+Negative numbers!
+Whole numbers (integers)!
+Decimal numbers!
+NaN (Not a Number) is a Number type (0/0) or (NaN + 10)
+Infinity (1 / 0)
+-Infinity (-1 / 0)
+-0
+------------------------------------------ */
+
+var positiveNum = 10;
+var negativeNum = -12;
+var integerNum = 112;
+var decimalNum = 3.2;
+var nanNum = 0 / 0;
+var InfinityNum = 1 / 0;
+var negativeInfinityNum = -1 / 0;
+var negativeZero = -0;
+
+/* ------------------------------------------ 
+Simple operation on numbers
+------------------------------------------ */
+// Addition
+console.log(10 + 5);
+
+// Substraction
+console.log(90 - 1);
+
+// Multiplication
+console.log(11 * 4);
+
+// Division
+console.log(10 / 3);
+
+// Modulo
+console.log(10 % 3);
+
+// Exponential
+console.log(10 ** 2);
