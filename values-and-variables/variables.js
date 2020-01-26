8ba6224aa1a0d55a307beed0cedcccdfe0cf1c9b
uname-sandeepb <59410723+uname-sandeepb@users.noreply.github.com>
2020-01-26 22:39:01 +0530

    const cannot be re assigned

--- values-and-variables/variables.js
+++ values-and-variables/variables.js
@@ -1,8 +1,19 @@
-// var
-var petName = "Homer";
-
-// let
+/*--------------------------- 
+let
+---------------------------*/
 let petAge = 4;
+petAge = 5;
 
-// const
+/*--------------------------- 
+Const 
+works just like let, except you cannot change the value
+Note:- Once we cover Arrays and Objects, we'll see other situations where const makes sense over let.
+---------------------------*/
 const petBreed = "Husky";
+// petBreed = 'pug' // Not Allowed
+
+/*--------------------------- 
+Var
+---------------------------*/
+var petName = "Homer";
+petName = "Max";
